fca497519190d49c40e3ee4a4e1bce77b498545d
edem021 <v.edem021@gmail.com>
2025-07-13 00:04:34 +0200

    fixed active page selection

--- frontend/src/components/SideMenu.jsx
+++ frontend/src/components/SideMenu.jsx
@@ -23,43 +23,53 @@ const SideMenu = () => {
   return (
     <nav className="nav-bar" style={{ height: "calc(100vh - 6rem)" }}>
       <ul className="flex flex-col gap-3">
-        <li
-          className={`nav-bar-btn ${
-            active === "home" ? "active-nav-bar-btn" : "hover:bg-base-100"
-          } `}
-          onClick={() => setActive("home")}
-        >
-          <BiSolidHome size={20} />
-          <Link to="/">Home</Link>
+        <li>
+          <Link
+            to="/"
+            className={`nav-bar-btn ${
+              active === "home" ? "active-nav-bar-btn" : "hover:bg-base-100"
+            }`}
+          >
+            <BiSolidHome size={20} />
+            Home
+          </Link>
         </li>
-        <li
-          className={`nav-bar-btn ${
-            active === "schedule" ? "active-nav-bar-btn" : "hover:bg-base-100"
-          }`}
-          onClick={() => setActive("schedule")}
-        >
-          <FaCalendarDays size={20} />
-          <Link to="/schedule">Schedule</Link>
+        <li>
+          <Link
+            to="/schedule"
+            className={`nav-bar-btn ${
+              active === "schedule" ? "active-nav-bar-btn" : "hover:bg-base-100"
+            }`}
+          >
+            <FaCalendarDays size={20} />
+            Schedule
+          </Link>
         </li>
-        <li
-          className={`nav-bar-btn ${
-            active === "latest-episodes"
-              ? "active-nav-bar-btn"
-              : "hover:bg-base-100"
-          }`}
-          onClick={() => setActive("latest-episodes")}
-        >
-          <FaClock size={20} />
-          <Link to="/latest-episodes">Latest Episodes</Link>
+        <li>
+          <Link
+            to="/latest-episodes"
+            className={`nav-bar-btn ${
+              active === "latest-episodes"
+                ? "active-nav-bar-btn"
+                : "hover:bg-base-100"
+            }`}
+          >
+            <FaClock size={20} />
+            Latest Episodes
+          </Link>
         </li>
-        <li
-          className={`nav-bar-btn ${
-            active === "song-list" ? "active-nav-bar-btn" : "hover:bg-base-100"
-          }`}
-          onClick={() => setActive("song-list")}
-        >
-          <FaMusic size={20} />
-          <Link to="/song-list">Song List</Link>
+        <li>
+          <Link
+            to="/song-list"
+            className={`nav-bar-btn ${
+              active === "song-list"
+                ? "active-nav-bar-btn"
+                : "hover:bg-base-100"
+            }`}
+          >
+            <FaMusic size={20} />
+            Song List
+          </Link>
         </li>
       </ul>
     </nav>
